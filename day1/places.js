264b584f508a02c61d5b83918632bbac515a3c58
Shiva Chandra T <shivachandra9490@gmail.com>
2025-07-11 10:05:54 +0530

    Add day1 folder

---
+++ day1/places.js
@@ -0,0 +1,14 @@
+const places = ['Tokyo',"seoul",'New york','Zurich']
+
+console.log(places);
+
+//module.exports = places;
+
+// For multiple datsets 
+
+const count =['Usa','Uk','France']
+
+
+module.exports = {
+    places,count
+}
